3e5052d91aa4f9fe5beb46a5be0b0d66725f4a8f
Shivansh Gupta <gupta.shivansh98@gmail.com>
2018-03-23 20:35:43 +0530

    Comment

--- src/posts/models.py
+++ src/posts/models.py
@@ -7,8 +7,11 @@ from django.db.models.signals import pre_save
 from django.utils import timezone
 from django.utils.safestring import mark_safe
 from django.utils.text import slugify
+from django.contrib.contenttypes.models import ContentType
+
 
 from markdown_deux import markdown
+from comments.models import Comment
 
 class PostManager(models.Manager):
 	def active(self, *args, **kwargs):
@@ -56,6 +59,19 @@ class Post(models.Model):
 
 	class Meta:
 		ordering = ["-timestamp","-updated"]
+
+	@property
+	def comments(self):
+		instance = self
+		qs = Comment.objects.filter_by_instance(instance)
+		return qs
+
+	@property
+	def get_content_type(self):
+		instance = self
+		content_type = ContentType.objects.get_for_model(instance.__class__)
+		return content_type
+		
 
 def create_slug(instance, new_slug=None):
 	slug = slugify(instance.title)
--- src/posts/views.py
+++ src/posts/views.py
@@ -8,6 +8,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils import timezone 
 
 from comments.models import Comment
+from comments.forms import CommentForm
+
 from .forms import PostForm
 from .models import Post
 # Create your views here.
@@ -37,16 +39,47 @@ def posts_detail(request, slug = None):
 		if not request.user.is_staff or not request.user.is_superuser:
 			raise Http404
 	share_string = quote_plus(instance.content)
-	comtent_type = ContentType.objects.get_for_model(Post)
-	obj_id = instance.id
-	comments = Comment.objects.filter(content_type=comtent_type, object_id=obj_id)
 
+	initial_data = {
+			"content_type": instance.get_content_type,
+			"object_id": instance.id
+	}
+
+	form = CommentForm(request.POST or None, initial = initial_data )
+	if form.is_valid():
+		c_type = form.cleaned_data.get("content_type")
+		content_type = ContentType.objects.get(model=c_type)
+		obj_id = form.cleaned_data.get('object_id')
+		content_data = form.cleaned_data.get("content")
+		parent_obj = None
+		try:
+			parent_id = int(request.POST.get("parent_id"))
+		except:
+			parent_id = None
+
+		if parent_id:
+			parent_qs = Comment.objects.filter(id=parent_id)
+			if parent_qs.exists() and parent_qs.count()==1:
+				parent_obj = parent_qs.first()
+
+		new_comment, created = Comment.objects.get_or_create(
+									user = request.user,
+									content_type= content_type,
+									object_id = obj_id,
+									content = content_data,
+									parent = parent_obj,
+									)
+		return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
+
+
+	comments = instance.comments #Comment.objects.filter_by_instance(instance)
 
 	context = {
 		"title" : instance.title,
 		"instance" : instance,
 		"share_string" : share_string,
 		"comments":comments,
+		"comment_form":form,
 	}
 	return render(request,"post_detail.html", context)
 
